c47af75d10b4d4187a9411a7baff1fb34e303be1
keppacco <latapiotrek@gmail.com>
2023-05-16 12:39:33 +0200

    dodano reszte kodu odpowiedzialna za zwracanie danych

--- src/Pracownik.java
+++ src/Pracownik.java
@@ -14,4 +14,27 @@ public class Pracownik {
         this.salary = salary;
         nextEmployeeID++;
     }
+
+    public int getEmployeeID() {
+        return employeeID;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", position='" + position + '\'' +
+                ", employeeID=" + employeeID +
+                ", salary=" + salary +
+                '}';
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
 }
