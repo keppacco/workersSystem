280a3cfc10cbc57cf0fb191a991369af1110fa74
keppacco <latapiotrek@gmail.com>
2023-05-16 12:35:43 +0200

    id dodaje się automatycznie

--- src/Pracownik.java
+++ src/Pracownik.java
@@ -1,2 +1,17 @@
 public class Pracownik {
+    private String firstName;
+    private String lastName;
+    private String position;
+    private int employeeID;
+    private int salary;
+    private static int nextEmployeeID;
+
+    public Pracownik(String firstName, String lastName, String position, int employeeID, int salary) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.position = position;
+        this.employeeID = nextEmployeeID;
+        this.salary = salary;
+        nextEmployeeID++;
+    }
 }
